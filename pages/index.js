3288cf8e5799c529dffe0ce01f4be963a1c93cca
Kinene <fahad.marascientific.org>
2022-09-13 08:55:36 +0300

    latest

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import Jelly from '../public/images/mukuzainstantplus.jpeg'
+import tin from '../public/images/tin.jpg'
 
 import Navbar from '../components/navbar'
 
@@ -91,13 +92,13 @@ export default function Home() {
 
       <div className={styles.products_section}>
             <div className={styles.product_section_container}>
-              <div>
+              <div className={styles.product_title}>
                 <h2>P Enlargement Products</h2>
                 <span>Choose the one product that fits your needs - we have tea and jelly</span>
               </div>
               <div className={styles.product_container}>
-                <div>
-                  <Image src={Jelly} width="200px" height="200px"/>
+                <div className={styles.product}>
+                  <Image src={tin} width="500px" height="300px"/>
                   <h4>Mukuza Instant Performance Tea </h4>
                   <span>(New Formula!)</span>
                   <span>(370.9$)</span>
@@ -119,51 +120,80 @@ export default function Home() {
                     Been Used All Over Africa from Sudan to Congo  -  
                     Highly Recommended! </p>
 
-                    <a>Get it Now</a>
+                    <a href='#'>Get it Now</a>
                 </div>
 
-                <div>
-                  <Image src={Jelly} width="200px" height="200px"/>
+                <div className={styles.product}>
+                  <Image src={Jelly} width="500px" height="300px"/>
                   <h4>Mukuza Instant Performance Tea </h4>
                   <span>(New Formula!)</span>
                   <span>(370.9$)</span>
-                  <p>Specially Formulated for those who want Extreme 
-                    Stallion like Performance - Mixed with over 
-                    15 secret highly potent Therapeutic herbs with 
-                    Aphrodisiac Properties that's been used across 
-                    Africa to treat different male Sexual Dysfunction 
-                    & Disorders for a very long time - For Masturbation 
-                    Victims, Low Sex Drive, Erectile Dysfunction,  
-                    Weak Erections & Sexual Stamina/Manpower (last in 
-                    bed for 45 minutes minimum), Increases Sperm Volume 
-                    & Tons of other health benefits - You simply mix 2 
-                    teaspoons in a cup full of hot water and let it settle 
-                    for about 5 mins, take twice a day in the morning and 
-                    afternoon for full effect and in just about 2 days 
-                    your entire system will undergo full detox and your 
-                    performance will start improving Instantly -  It's 
-                    Been Used All Over Africa from Sudan to Congo  -  
-                    Highly Recommended! </p>
+                  <p>Our Flagship Product - The Ultimate All in One Penis 
+                    Enlargement Solution for Rapid Penis Length and Girth -
+                    This Updated Formula increases absorption of the herbs 
+                    therapeutic properties directly onto the erectile tissue 
+                    and blood vessels for maximum growth in size - Mixed 
+                    with over 10 rare Herbs only known to a few people, 
+                    like the Magejo Plant, Mugolola Tree, Mukuza Roots - 
+                    Instructions: You first apply a warm wet towel on your 
+                    penis for a few seconds to open the foreskin pores, and 
+                    relax the penis muscles then simply massage the jelly on 
+                    your penis while stretching/pulling forwards for about 
+                    3 mins twice a day and in about 14 days you will have 
+                    achieved significant gains both length and girth. 
+                    (NOTE: you must stop when you get your desired size - 
+                    side effects of over using this jelly are irreversible)</p>
 
-                    <a>Get it Now</a>
+                    <a href='#'>Get it Now</a>
                 </div>
             </div>
             </div>
       </div>
 
-      <div>
-        <div>
+      <div className={styles.extra}>
+        <div className={styles.shipping}>
           <h1>Free Worldwide shipping Via DHL</h1>
         </div>
-        <div>
-          <h1>How to Order!</h1>
-          <ol>
-            <l1>Choose your Intended product</l1>
-            <li>Send us a message via WhatsApp or Email</li>
-            <li>Include Shipping Information to Avoid Delay.</li>
-          </ol>
+        <div className={styles.order_now}>
+          <div className={styles.order_now_container}>
+            <h1>How to Order!</h1>
+            <ol>
+              <li>Choose your Intended product</li>
+              <li>Send us a message via WhatsApp or Email</li>
+              <li>Include Shipping Information to Avoid Delay.</li>
+            </ol>
+          </div>
+          
         </div>
       </div>
+
+      <div className={styles.qoute}>
+
+        <div className={styles.qoute_container}>
+          <h3>Some of Our Satisfied Clients</h3>
+          <p>
+            <q>Been using it for about a 11 days 
+              now and i can see a very huge increase 
+              in girth when I'm flaccid and erect. 
+              I've been massaging it in for about 
+              2 min, but the bottle suggests 5 mins 
+              so maybe that's why I'm seeing slower 
+              results. Overall, it's just unbelievable 
+              what i am seeing - i can't believe this 
+              is part of my body!.</q>
+          </p>
+        </div>
+
+      </div>
+
+      <div className={styles.talk_section}>
+        <h3>Get The Size of Your Dreams Now!!!</h3>
+        <a href='#'>Talk To Us Via Whatsapp</a>
+      </div>
+
+      <footer>
+
+      </footer>
       
     </div>
   )
